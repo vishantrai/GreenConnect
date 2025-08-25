ee28932ab53af389bfff1d4756befe1f8dae9d1e
vishantrai <raivishantkumar@gmail.com>
2025-08-25 19:58:50 +0530

    models changed for posts and created for multiple type of posts

--- backend/models.py
+++ backend/models.py
@@ -21,7 +21,7 @@ class Address(Base):
 
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
-    user = relationship("User")
+    user = relationship("User") 
     address_line = Column(String, nullable=True)
     locality = Column(String, nullable=False)
     city = Column(String, nullable=False)
@@ -30,6 +30,7 @@ class Address(Base):
     pincode = Column(String, nullable=False)
     latitude = Column(NUMERIC(12,6), nullable=False)
     longitude = Column(NUMERIC(12,6), nullable=False)
+    created_at=Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
 
 class Role(Base):
     __tablename__="roles"
@@ -42,17 +43,27 @@ class Role(Base):
     donor= Column(Boolean, nullable=False, default=False) #mention on frontend as this is plant donor, equipment donor, labour donor because we dont allow direct money donation
     land_provider= Column(Boolean, nullable=False, default=False) 
 
-class LandDonationPost(Base):
-    __tablename__ = "land_donation_post"
+# post tables
+class Post(Base):
+    __tablename__ = "posts"
 
     id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False ) 
-    donor_type = Column(String, nullable=False )
+    title = Column(String, nullable=False)
     mobile_number = Column(String, nullable=False) #mobile should not be unique because one number can be used for multiple posts
     email= Column(String, nullable=True)
     address = Column(String, nullable=False)
     latitude = Column(Float, nullable= False)
     longitude = Column(Float, nullable= False)
+    post_type = Column(String, nullable = False) 
+    description = Column(String, nullable=False)
+    image_url = Column(String, nullable=False)
+    created_at=Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
+
+class LandDonationPost(Base):
+    __tablename__ = "land_donations"
+
+    id = Column(Integer, ForeignKey("posts.id"), primary_key=True)
     area = Column(Float, nullable=False)
     area_unit = Column(String, nullable=False)
     soil_type = Column(String, nullable=False)
@@ -64,31 +75,21 @@ class LandDonationPost(Base):
     road_access = Column(Boolean, nullable=False)
     fencing = Column(Boolean, nullable=False)
     water_source = Column(Boolean, nullable=False)
-    image_url = Column(String, nullable=False)
-    special_note = Column(String, nullable=False)
 
-class Supporters(Base):
-    __tablename__="supporters"
-
-    id = Column(Integer, primary_key=True)
-    name = Column(String, nullable=False ) 
-    donor_type = Column(String, nullable=False )
-    mobile_number = Column(String, nullable=False) #mobile should not be unique because one number can be used for multiple posts
-    email= Column(String, nullable=True)
 
 class SaplingDonor(Base):
     __tablename__ = "sapling_donor"
 
-    user_id = Column(Integer, ForeignKey("supporters.id"), primary_key=True)
+    id = Column(Integer, ForeignKey("posts.id"), primary_key=True)
     tree_type = Column(String, nullable=True)
     quantity = Column(Integer, nullable=True)
-    special_note = Column(String, nullable=True)
+
 
 class EquipmentDonor(Base):
 
     __tablename__ = "equipment_donor"
 
-    user_id = Column(Integer, ForeignKey("supporters.id"), primary_key=True)
+    id = Column(Integer, ForeignKey("posts.id"), primary_key=True)
     spades = Column(Boolean, nullable=True)
     equipment_num1 = Column(Integer, nullable=True)
 
@@ -107,16 +108,29 @@ class EquipmentDonor(Base):
 class LabourDonor(Base):
     __tablename__ = "labour_donor " 
 
-    user_id = Column(Integer, ForeignKey("supporters.id"), primary_key=True)
+    id = Column(Integer, ForeignKey("posts.id"), primary_key=True)
     no_of_people = Column(Integer, nullable=True, default= 5)
     availabilty = Column(TIMESTAMP(timezone=True),nullable=False)
 
 class LogisticHelp(Base):
-
     __tablename__= "logistic help"
-    user_id = Column(Integer, ForeignKey("supporters.id"), primary_key=True)
+
+    id = Column(Integer, ForeignKey("posts.id"), primary_key=True)
     mode_of_transport = Column(String, nullable=True)
     max_distance = Column(Integer, nullable=True, default= 10)
+
+class CareRequests(Base):
+    __tablename__ = "care_requests"
+
+    id = Column(Integer, ForeignKey("posts.id"), primary_key=True)
+    tree_type = Column(String, nullable=False)
+    current_condition = Column(String, nullable=False)
+    water_need = Column(Boolean)
+    fence_need = Column(Boolean)
+    pest_control = Column(Boolean)
+    physical_damage = Column(Boolean)
+    soil_health = Column(String)
+
 
 #notes
 #we cannot define more than one table in one class of the sqlalchemy so for each table create different class
