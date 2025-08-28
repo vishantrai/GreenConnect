8be7b32c00e4c8e4805baeab4771040a8a5696e7
vishantrai <raivishantkumar@gmail.com>
2025-08-28 23:17:09 +0530

    login route is enabled

--- backend/main.py
+++ backend/main.py
@@ -3,7 +3,7 @@ import psycopg2
 from psycopg2.extras import RealDictCursor
 import models
 from database import Base, engine
-import user, posts
+import user, posts, auth
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
@@ -20,3 +20,4 @@ while True:
 
 app.include_router(user.router)
 app.include_router(posts.router)
+app.include_router(auth.router)
