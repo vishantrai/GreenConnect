fd72aaa99c11d730e19f8315b0d6f27bee2fa4b3
vishantrai <raivishantkumar@gmail.com>
2025-08-30 20:06:13 +0530

    environment variables created

---
+++ backend/config.py
@@ -0,0 +1,18 @@
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    database_hostname: str
+    database_port: str
+    database_password: str
+    database_name: str
+    database_username: str
+    secret_key: str
+    algorithm: str
+    access_token_expire_minutes: int
+
+    class Config:
+        env_file= "D:\System\GreenConnect\.env"
+
+
+settings = Settings()
+# print("Loaded hostname:", settings.database_hostname) just for test purpose
