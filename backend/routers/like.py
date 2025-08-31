95f36544ccdbaa0fdadd5a2d88d61cea15e47470
vishantrai <raivishantkumar@gmail.com>
2025-08-31 13:45:53 +0530

    Alembic installed

--- backend/routers/like.py
+++ backend/routers/like.py
@@ -10,6 +10,11 @@ router = APIRouter(
 
 @router.post("/", status_code=status.HTTP_201_CREATED)
 def like(like: schemas.Like, db: Session = Depends(database.get_db), current_user: int = Depends(oauth2.get_current_user)):
+
+    post = db.query(models.Post).filter(models.Post.id == like.post_id).first()
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id: {like.post_id} does not exist")
+
     like_query = db.query(models.Like).filter(models.Like.post_id == like.post_id, models.Like.user_id == current_user.id)
     found_like = like_query.first()
 
@@ -28,3 +33,10 @@ def like(like: schemas.Like, db: Session = Depends(database.get_db), current_use
         db.commit()
 
         return {"message": "successfully deleted like"}
+    
+
+
+# notes
+# we want that when ever a post is fetched on the frontend the total number of likes on the post should be seen there 
+# join - join two tables
+
